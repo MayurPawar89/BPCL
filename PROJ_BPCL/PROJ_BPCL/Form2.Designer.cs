9a3dee17d6fed6d0c73b8c14566fd8c380c9cfdc
Mayur Office <mayurpawar89@hotmail.com>
2016-08-10 13:03:04 +0530

    Solution added

---
+++ PROJ_BPCL/PROJ_BPCL/Form2.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PROJ_BPCL
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtMachineName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(133, 91);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(96, 27);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Generate";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtMachineName
+            // 
+            this.txtMachineName.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMachineName.Location = new System.Drawing.Point(149, 27);
+            this.txtMachineName.Name = "txtMachineName";
+            this.txtMachineName.Size = new System.Drawing.Size(182, 22);
+            this.txtMachineName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Machine Name : ";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(149, 53);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(182, 22);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 14);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Password : ";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(235, 91);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(96, 27);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(29, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(57, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(308, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "* Mandatory Fields";
+            // 
+            // Form2
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 130);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtMachineName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Key Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtMachineName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ PROJ_BPCL/PROJ_BPCL/Form2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DBLayer;
+
+namespace PROJ_BPCL
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            txtMachineName.Text = Environment.MachineName;
+            if (txtMachineName.Text.Trim() != "")
+            {
+                txtPassword.Select();
+                txtPassword.Focus();
+            }
+        }
+        DBAccess dbAccessLayer = null;
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (txtMachineName.Text.Trim() != "")
+            {
+                if (txtPassword.Text.Trim().ToString()=="admin123")
+                {
+                    dbAccessLayer = new DBAccess();
+                    string sInputKey = Convert.ToString(txtMachineName.Text.Trim().ToUpper()) + "-"+Convert.ToString(txtPassword.Text.Trim());
+                    string sEncryptedKey=Encryption.EncryptToBase64String(sInputKey);
+                    if (InsertEncryptedKeyInDB(sEncryptedKey))
+                    {
+                        MessageBox.Show(string.Format("{0} is register with product and able to use the product.", Convert.ToString(txtMachineName.Text.Trim())), "Key Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        //MessageBox.Show("Re-start the application", "Key Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        //Application.Exit();
+                        
+                        this.ShowInTaskbar = false;
+                        this.Hide();
+                        Form1 ofrmSearch = new Form1();
+                        ofrmSearch.WindowState = FormWindowState.Maximized;
+                        ofrmSearch.ShowDialog(this);
+                        ofrmSearch.Dispose();
+                        Application.Exit();
+
+                    }
+                    else
+                        MessageBox.Show(string.Format("Either \"{0} is already register with product\" Or \"Error occured in {0} register process with product\".\nContact your application vendor.", Convert.ToString(txtMachineName.Text.Trim())), "Key Generator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                    MessageBox.Show(string.Format("You have not provided the password or password you have enterd doesn't match. Contact your application vendor."), "Key Generator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Machine Name is required."), "Key Generator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool InsertEncryptedKeyInDB(string sEncryptedKey)
+        {
+            bool _result = false;
+            try
+            {
+                _result= dbAccessLayer.InsertProductKey(sEncryptedKey);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in product registration: " + ex.Message, "Key Generator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+
+            }
+
+            return _result;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMachineName.Clear();
+            txtPassword.Clear();
+        }
+    }
+}
